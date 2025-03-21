6920ab7f6f76dccfea19c4f3f38c7790991d0ce1
Lucas Hou <lmail888888@gmail.com>
2025-03-21 01:06:01 -0700

    Exhaustive and probabilistic group member selection

--- tests/test_topo_geo_hyperbolic.py
+++ tests/test_topo_geo_hyperbolic.py
@@ -6,45 +6,48 @@ sys.path.append('../src')
 
 from simulator import *
 from routing import *
+from powerset import *
 
 def main():
 
     # Get config file, parse and turn into routing info via Bellman-Ford
-    group_members = [0, 2, 4]
     config_file = "topo_geo_hyperbolic.conf"
-    
-    nodes, routes = converge(group_members, config_file)
-
-    print(routes)
-
-    for s in strategy_map:
-        strategy = strategy_map[s]
-        print(f'Strategy {strategy.name}:')
-        print()
-        sim = Simulator(len(nodes), strategy)
-        sim.add_to_group(group_members, '/test')
-
-        # This would normally be built by a distance vector router
-        for r in routes:
-            #print(r, routes[r])
-            sim.set_routes(r, routes[r])
-
-        starting_interests = [([0], '/test')]
-
-        produced, dropped, kept, sent = sim.run(starting_interests)
-
-        print()
-        print("SUMMARY")
-        for i in range(len(nodes)):
-            print(f'Node {i} produced {produced[i]}, dropped {dropped[i]}, kept {kept[i]}, sent {sent[i]}')
+    group_members_combination = powerset(5)
+    for group_members in group_members_combination:
+        print(group_members)
         
-        print()
-        print(f'Total produced: {sum(produced)}, total dropped: {sum(dropped)}, total kept: {sum(kept)}, total sent: {sum(sent)}')
-
-        print()
-        print()
-
+        
     
+        nodes, routes = converge(group_members, config_file)
+
+        print(routes)
+
+        for s in strategy_map:
+            strategy = strategy_map[s]
+            print(f'Strategy {strategy.name}:')
+            print()
+            sim = Simulator(len(nodes), strategy)
+            sim.add_to_group(group_members, '/test')
+
+            # This would normally be built by a distance vector router
+            for r in routes:
+                #print(r, routes[r])
+                sim.set_routes(r, routes[r])
+
+            starting_interests = [([group_members[0]], '/test')]
+
+            produced, dropped, kept, sent = sim.run(starting_interests)
+
+            print()
+            print("SUMMARY")
+            for i in range(len(nodes)):
+                print(f'Node {i} produced {produced[i]}, dropped {dropped[i]}, kept {kept[i]}, sent {sent[i]}')
+            
+            print()
+            print(f'Total produced: {sum(produced)}, total dropped: {sum(dropped)}, total kept: {sum(kept)}, total sent: {sum(sent)}')
+
+            print()
+            print()
 
     print("Simulation complete!")
 
